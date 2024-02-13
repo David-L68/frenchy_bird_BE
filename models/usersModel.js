f0cc0a89159f222b59e273a45aba202d70d962ee
David Lasry <davidlasry2000@gmail.com>
2024-02-13 21:30:05 +0200

    commit

---
+++ models/usersModel.js
@@ -0,0 +1,44 @@
+const User = require("../models/User");
+
+const getUserByEmailModel = async (email) => {
+  try {
+    const user = await User.findOne({ email: email });
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getUserByNicknameModel = async (nickname) => {
+  try {
+    const user = await User.findOne({ nickname: nickname });
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const addUserModel = async (newUser) => {
+  try {
+    const user = await User.create(newUser);
+    return user;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllUsersModel = async () => {
+  try {
+    const allUsers = await User.find();
+    return allUsers;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  getUserByEmailModel,
+  addUserModel,
+  getUserByNicknameModel,
+  getAllUsersModel,
+};
